ab0dfecb90e28e5d9df4e46095c32a0a4d8e4dfb
s26532 <s26532@pjwstk.edu.pl>
2024-12-04 08:34:05 +0100

    Implementacja metody wyliczającą średnią

--- Program.cs
+++ Program.cs
@@ -3,3 +3,19 @@ Console.WriteLine("Hello, World!");
 Console.WriteLine("Modyfikacja 1");
 Console.WriteLine("Modyfikacja 2");
 Console.WriteLine("Modyfikacja 3");
+
+var values = new[] {9, 1, 5, 0};
+Console.WriteLine($"Średnia dla [{String.Join(", ", values)}] to: {CalculateAverage(values)}");
+
+
+static double CalculateAverage(int[] values)
+{
+    var result = 0;
+
+    for(int i = 0; i < values.Length; i++)
+    {
+        result += values[i];
+    }
+
+    return result / values.Length;
+}
