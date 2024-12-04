011788ab7efb4ec0a0343d5d06eb67a74c3baecf
s26532 <s26532@pjwstk.edu.pl>
2024-12-04 08:40:32 +0100

    Implementacja metody wyliczającej maksimum

--- Program.cs
+++ Program.cs
@@ -6,7 +6,7 @@ Console.WriteLine("Modyfikacja 3");
 
 var values = new[] {9, 1, 5, 0};
 Console.WriteLine($"Średnia dla [{String.Join(", ", values)}] to: {CalculateAverage(values)}");
-
+Console.WriteLine($"Maksimum dla [{String.Join(", ", values)}] to: {CalculateMaximum(values)}");
 
 static double CalculateAverage(int[] values)
 {
@@ -18,4 +18,16 @@ static double CalculateAverage(int[] values)
     }
 
     return result / values.Length;
+}
+
+static int CalculateMaximum(int[] values)
+{
+    var result = int.MinValue;
+
+    for(int i = 0; i < values.Length; i++)
+    {
+        result = values[i] > result ? values[i] : result;
+    }
+
+    return result;
 }
